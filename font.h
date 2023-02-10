0717b32342637dab193ae4393f29a8fc887c0edd
G6EJD <d.l.bird@sky.com>
2023-02-10 10:13:51 +0000

    Update font.h

--- font.h
+++ font.h
@@ -1,6 +1,6 @@
 // Created by http://oleddisplay.squix.ch/ Consider a donation
 // In case of problems make sure that you are using the font file with the correct version!
-const char Dialog_plain_8[] PROGMEM = {
+const uint8_t Dialog_plain_8[] PROGMEM = {
   0x08, // Width: 8
   0x0A, // Height: 10
   0x20, // First Char: 32
